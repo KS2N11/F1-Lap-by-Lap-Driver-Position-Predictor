b48af35bbdb0835cdcead3dd3d8c63c9a6a8fd7e
Khush Shah <113935688+KS2N11@users.noreply.github.com>
2024-10-21 19:01:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,47 @@ with st.expander('Raw Data'):
   lap_times
   st.write('**Results**')
   results
+
+
+circuits = circuits.drop(columns = ['lat', 'lng', 'alt', 'url'], axis = 0)
+races = races.drop(columns = ['date', 'time', 'url', 'fp1_date', 'fp1_time', 'fp2_date', 'fp2_time', 'fp3_date', 'fp3_time', 'quali_date', 'quali_time', 'sprint_date', 'sprint_time'], axis = 0)
+
+merge1 = circuits.merge(races, on = 'circuitId', how = 'right')
+merge1 = merge1.drop(columns = ['circuitRef', 'name_x', 'location', 'country', 'name_y'], axis = 0)
+
+drivers = drivers.drop(columns = ['number', 'dob', 'url'], axis = 0)
+driver_standings = driver_standings.rename(columns = {'position': 'driver_fin_pos'})
+driver_standings = driver_standings.drop(columns = ['positionText', 'wins'], axis = 0)
+
+merge2 = drivers.merge(driver_standings, on = 'driverId', how = 'right')
+merge2 = merge2.drop(columns = ['driverRef', 'code', 'forename', 'surname', 'nationality', 'driverStandingsId', 'points'], axis = 0)
+
+constructors = constructors.drop(columns = ['url', 'nationality'], axis = 0)
+constructor_standings = constructor_standings.rename(columns = {'position': 'constructor_fin_pos'})
+constructor_standings = constructor_standings.drop(columns = ['positionText', 'wins'], axis = 0)
+
+merge3 = constructors.merge(constructor_standings, on = 'constructorId', how = 'right')
+merge3 = merge3.drop(columns = ['constructorRef', 'name', 'constructorStandingsId', 'points'], axis = 0)
+
+merge1_2 = merge1.merge(merge2, on = 'raceId', how = 'left')
+merge1_2_3 = merge1_2.merge(merge3, on = 'raceId', how = 'left')
+
+lap_times = lap_times.rename(columns = {'position': 'driver_curr_pos', 'lap': 'curr_lap', 'time': 'curr_lap_time'})
+
+merge4 = merge1_2_3.merge(lap_times, on = ['raceId', 'driverId'], how = 'left')
+
+results = results.rename(columns = {'position': 'driver_fin_pos', 'grid': 'driver_start_pos', 'time': 'fin_time', 'rank': 'driver_fastest_lap_rank', 'laps': 'total_laps'})
+results = results.drop(columns = ['number', 'positionText', 'positionOrder', 'points', 'milliseconds', 'fin_time', 'fastestLap', 'driver_fastest_lap_rank', 'fastestLapTime', 'fastestLapSpeed', 'statusId'], axis = 0)
+
+merge4['driver_fin_pos'] = pd.to_numeric(merge4['driver_fin_pos'], errors='coerce') # Convert to numeric, handling errors
+results['driver_fin_pos'] = pd.to_numeric(results['driver_fin_pos'], errors='coerce') # Convert to numeric, handling errors
+
+finalmerge = merge4.merge(results, on=['raceId', 'driverId', 'constructorId', 'driver_fin_pos'], how='right')
+finalmerge = finalmerge.drop(columns = ['curr_lap_time', 'resultId', 'constructor_fin_pos', 'total_laps', 'milliseconds'])
+
+dataset = finalmerge
+dataset.dropna(subset=['driver_curr_pos'], inplace=True)
+
+dataset
+
+
