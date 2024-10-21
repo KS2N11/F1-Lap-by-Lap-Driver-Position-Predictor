86abc9c6e47a24fa5e227a1f44af08c2ea0ce608
Khush Shah <113935688+KS2N11@users.noreply.github.com>
2024-10-21 23:59:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
 
-# CSS to Position the Logo at the Top-Left Corner
+# CSS to Center the Logo at the Top
 st.markdown(
     """
     <style>
@@ -14,11 +14,12 @@ st.markdown(
     [data-testid="stHeader"] {
         background: rgba(0, 0, 0, 0);
     }
-    .top-left-logo {
-        position: absolute;
-        top: 10px;
-        left: 10px;
-        width: 100px;
+    .center-logo {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 150px;
+        margin-top: 10px;
     }
     </style>
     """,
@@ -29,7 +30,7 @@ st.markdown(
 st.markdown(
     """
     <a href="https://www.formula1.com/" target="_blank">
-        <img class="top-left-logo" src="https://upload.wikimedia.org/wikipedia/commons/3/33/F1.svg">
+        <img class="center-logo" src="https://upload.wikimedia.org/wikipedia/commons/3/33/F1.svg">
     </a>
     """,
     unsafe_allow_html=True,
